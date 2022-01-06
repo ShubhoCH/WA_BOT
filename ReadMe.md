45056609ea88f13dd5708baaaa029e5060df1ca0
SHUBHO CHAKRABORTY <shubho1chakraborty1@gmail.com>
2022-01-06 12:59:19 +0530

    Added ReadMe.md

---
+++ ReadMe.md
@@ -0,0 +1,19 @@
+# WA BOT
+
+Interact with customers via WhatsApp!
+
+## Installation
+
+Clone the project from github then go to project directory, inside the project and type the following command to install all the required dependencies for the project!
+
+```bash
+npm i
+```
+
+## How to Use:
+ 
+In the app.js File call the 
+```bash
+Karix.send(<payload_content>, <phno>)
+```
+function with the requred payload template and Receiver's Phno.
--- app.js
+++ app.js
@@ -5,36 +5,37 @@ Karix.send(
     {
         "preview_url":false,
         "type":"TEXT",
-        "text":"Hey6!"
+        "text":"Hey, Shubho!"
     },
     "918757014613"
 );
 
-//Send Auto-Template Messages:
-Karix.send(
-    {
-        "preview_url": false,
-        "type": "AUTO_TEMPLATE",
-        "text": "Hi Shubho! Thanks for starting your personal training plan. We’ll send you a weekly update with your new schedule. You can log-in online using your training ID 123456. Stay fit !"
-    },
-    "918757014613"
-);
+// //Send Auto-Template Messages:
+// Karix.send(
+//     {
+//         "preview_url": false,
+//         "type": "AUTO_TEMPLATE",
+//         "text": "Hi Shubho! Thanks for starting your personal training plan. We’ll send you a weekly update with your new schedule. You can log-in online using your training ID 123456. Stay fit !"
+//     },
+//     "918757014613"
+// );
 
-//Send Template Messages:
-Karix.send(
-    {
-        "preview_url": false,
-        "type": "TEMPLATE",
-        "template":{
-            "templateId": "first_order_review",
-            "parameterValues": {
-                "0": "Shubho"
-            }
-        }
-    },
-    "918757014613"
-);
+// //Send Template Messages:
+// Karix.send(
+//     {
+//         "preview_url": false,
+//         "type": "TEMPLATE",
+//         "template":{
+//             "templateId": "first_order_review",
+//             "parameterValues": {
+//                 "0": "Shubho"
+//             }
+//         }
+//     },
+//     "918757014613"
+// );
 
+// // Media:
 // Karix.send(
 //     {
 //         "preview_url": false,
